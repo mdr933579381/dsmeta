1bad23204a773627ecd66537a2756950b26d0331
Marcio Del Raso <marciodr2m@gmail.com>
2022-07-12 17:02:47 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
-function App() {
-  return (
-    <h1>Olá Mundo!</h1>
+import NotificationButton from './componentes/NotificationButton'
 
-  )  
-}
+
+  function App() {
+    return (<>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+    )
+  }
 
 export default App
